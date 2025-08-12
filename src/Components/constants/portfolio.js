3ec988f778348521df3aaeb08d6b8c9dbc06e72f
Ankita Mishra <115132134+Ankitamishra17@users.noreply.github.com>
2025-08-12 16:23:27 +0530

    fixs

--- src/Components/constants/portfolio.js
+++ src/Components/constants/portfolio.js
@@ -2,8 +2,8 @@ import img5 from "../../assets/portfolio/img5.png";
 import reverse_string from "../../assets/portfolio/reverse_string.jpg";
 import translator from "../../assets/portfolio/translator.png";
 import netflix from "../../assets/portfolio/netflix.jpg";
-// import whatsapp from "../../assets/portfolio/whatsapp.png";
 import nspay from "../../assets/portfolio/nspay.png";
+import whatsapp from "../../assets/portfolio/whatsapp.png";
 
 export const ports = [
   {
@@ -36,10 +36,10 @@ export const ports = [
     demo: "https://nspay.vercel.app/",
     code: "https://github.com/Ankitamishra17/NsPay",
   },
-  // {
-  //   id: 6,
-  //   src: whatsapp,
-  //   demo: "https://github.com/Ankitamishra17/WhatsApp-Chat-Analyzer",
-  //   code: "https://github.com/Ankitamishra17/WhatsApp-Chat-Analyzer",
-  // },
+  {
+    id: 6,
+    src: whatsapp,
+    demo: "https://github.com/Ankitamishra17/WhatsApp-Chat-Analyzer",
+    code: "https://github.com/Ankitamishra17/WhatsApp-Chat-Analyzer",
+  },
 ];
